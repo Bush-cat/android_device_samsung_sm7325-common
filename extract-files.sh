b5be777f8beec3346d9601bba5b1e646be9739cf
electimon <electimon@gmail.com>
2024-04-18 10:08:15 +0700

    sm7325-common: Move sscrpcd to class core
    * If sscrpcd start early, the dsp it runs on
      isn't ready yet.
    
    test: Notice reduction of
          "Error 0x200: apps_dev_init failed for domain 0, errno No such device"
          in early boot logcat.
    Change-Id: Ib3ec232677e4831262de631f5ccd9dfa6008f6a0
    Signed-off-by: electimon <electimon@gmail.com>

--- extract-files.sh
+++ extract-files.sh
@@ -84,6 +84,10 @@ function blob_fixup() {
             [ "$2" = "" ] && return 0
             "${PATCHELF}" --add-needed "libui_shim.so" "${2}"
             ;;
+        vendor/etc/init/vendor.sensors.sscrpcd.rc)
+            [ "$2" = "" ] && return 0
+            sed -i 's|class early_hal|class core|g' "${2}"
+            ;;
         *)
             return 1
             ;;
