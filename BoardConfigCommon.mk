cf17fc6a516277f15f2aa47d0cc6f41e601d8706
LuK1337 <priv.luk@gmail.com>
2024-08-28 15:32:59 +0200

    sm7325-common: Migrate to soong_config_set()
    Change-Id: I06de8759836114f54a822d6f23f1177515e21178

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -155,17 +155,6 @@ AUDIO_FEATURE_ENABLED_SSR := false
 AUDIO_FEATURE_ENABLED_SVA_MULTI_STAGE := true
 BOARD_SUPPORTS_SOUND_TRIGGER := true
 
-# Camera
-SOONG_CONFIG_NAMESPACES += samsungCameraVars
-SOONG_CONFIG_samsungCameraVars += \
-    extra_ids \
-    needs_sec_reserved_field
-
-# ID=54 is macro
-SOONG_CONFIG_samsungCameraVars_extra_ids := 54
-
-SOONG_CONFIG_samsungCameraVars_needs_sec_reserved_field := true
-
 # Init
 TARGET_INIT_VENDOR_LIB := //$(COMMON_PATH):libinit_samsung_sm7325
 
--- common.mk
+++ common.mk
@@ -114,6 +114,9 @@ PRODUCT_PACKAGES += \
     android.hardware.bluetooth@1.0.vendor
 
 # Camera
+$(call soong_config_set,samsungCameraVars,needs_sec_reserved_field,true)
+$(call soong_config_set,samsungCameraVars,extra_ids,54)
+
 PRODUCT_PACKAGES += \
     android.hardware.camera.provider-service.samsung \
     libgrallocusage.vendor \
