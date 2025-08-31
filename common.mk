070ec825c1be839ae9f8e312313d0e02f38fdf7d
Bush-cat <73399885+Bush-cat@users.noreply.github.com>
2025-08-31 17:23:30 +0000

    sm7325-common: Move powerhint.json to config/power/

--- common.mk
+++ common.mk
@@ -337,7 +337,7 @@ PRODUCT_PACKAGES += \
     android.hardware.power@1.2.vendor
 
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json
+    $(LOCAL_PATH)/configs/power/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json
 
 # QCOM
 PRODUCT_PACKAGES += \
